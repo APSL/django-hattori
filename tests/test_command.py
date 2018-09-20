6384646de7cc1a9ca68f2c4b69a124eaf45b0928
Andreu Vallbona <avallbona@apsl.net>
2018-09-20 18:48:44 +0200

    implementing feature

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,12 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 import pytest
+
 from model_mommy import mommy
-
-from django.core.management import call_command
 from model_mommy.recipe import seq
-
 from tests.models import Person
+from django.core.management import call_command
 
 
 @pytest.mark.django_db
@@ -14,12 +13,13 @@ class TestCommand(object):
 
     @pytest.fixture
     def data(self):
-        return mommy.make(Person, name=seq('name'), surname=seq('surname-'), _quantity=1000)
+        return mommy.make(Person, name=seq('name-'), surname=seq('surname-'), _quantity=1000)
 
     def test_prova(self, data):
         assert True
         assert Person.objects.count() == 1000
-        print(Person.objects.last())
+        last = Person.objects.last()
+        assert last.name == 'name-1000'
 
     # def test_simple_command(self, data):
     #     call_command('anonymize_db')
